39c9d873f5d49d3302529a32a5ce35aaa347b1ad
Joachim Schiele <js@lastlog.de>
2016-12-09 16:24:12 +0100

    added build.rs support

--- nix-crates-index/all-carg-packages.nix
+++ nix-crates-index/all-carg-packages.nix
@@ -32,10 +32,20 @@ let
         # FIXME will fail for packages with names containing a '-' when they are replaced by '_'
         cd ${name}-${version}
       '';
+
+      OUT_DIR="OUT_DIR is not set properly, please fix the nix expression in question..."; # init env variable
+      CARGO_PKG_NAME=nameFix;
+      CARGO_PKG_VERSION=version;
+      CARGO_PKG_HOMEPAGE="";
+      CARGO_PKG_AUTHORS="";
+      CARGO_PKG_DESCRIPTION="";
+
       buildPhase = ''
 
         echo "${name} - ${depsString}"
         echo "namefix ${nameFix}"
+        export OUT_DIR=$(mktemp -d --tmpdir nix-output.XXXXXX)
+
         ${symlinkCalc cratesDeps}
         du -ha
         echo "name ${name}"
@@ -45,15 +55,78 @@ let
         else
           S=""
         fi
+#         export OUT_DIR=$(mktemp -d --tmpdir nix-output.XXXXXX)
 #        ${pkgs.rustc}/bin/rustc --crate-type=lib ''${S}lib.rs ${depsString} -o lib${nameFix}.rlib --crate-name ${nameFix} --cfg "feature=\"unsupported\"" --cfg "feature=\"default\""
 #        ${pkgs.rustc}/bin/rustc --crate-type=lib ''${S}lib.rs -o lib${nameFix}.rlib --crate-name ${nameFix} --cfg "feature=\"unsupported\"" --cfg "feature=\"default\"" -L dependency=mylibs
 #        ${pkgs.rustc}/bin/rustc --crate-type=lib ''${S}lib.rs -o lib${nameFix}.rlib --crate-name ${nameFix} --cfg "feature=\"use_std\"" -L dependency=mylibs
-        ${pkgs.rustc}/bin/rustc --crate-type=lib ''${S}lib.rs -o lib${nameFix}.rlib ${depsString} --crate-name ${nameFix} --cfg "feature=\"use_std\"" -L dependency=mylibs
+#         OUT_DIR=/nix-output1 
+#         export OUT_DIR=./nix-output2
+#         export | grep OUT_DIR
+   
+        # if a rust build script is around we do strange things!
+        if [ -f "build.rs" ]; then
+          echo "------- build.rs found: $name ----------"
+          ${pkgs.rustc}/bin/rustc build.rs --crate-name build_script_build --crate-type "bin" ${depsString} --cap-lints "allow"  -L dependency=mylibs --cfg "feature=\"default\"" --cfg "feature=\"std\"" -o build-script-build
+          du -ha
+          ./build-script-build
+#https://nopaste.me/view/b6633ea2
+
+#["rustc", "/home/joachim/.cargo/registry/src/github.com-1ecc6299db9ec823/target_build_utils-0.1.2/build.rs", "--crate-name", "build_script_build", "--crate-type", "bin", "-g", "--out-dir", "/home/joachim/Desktop/projects/fractalide/fetchUrl/hello_flate2/target/debug/build/target_build_utils-d8a53fa4b38f6bc1", "--emit=dep-info,link", "-L", "dependency=/home/joachim/Desktop/projects/fractalide/fetchUrl/hello_flate2/target/debug/deps", "-L", "dependency=/home/joachim/Desktop/projects/fractalide/fetchUrl/hello_flate2/target/debug/deps", "--extern", "phf_codegen=/home/joachim/Desktop/projects/fractalide/fetchUrl/hello_flate2/target/debug/deps/libphf_codegen-8c5dc4c06cc5b2f5.rlib", "--cap-lints", "allow"], [/* 106 vars */]) = 0
+          du -ha $OUT_DIR/
+          echo "------- /build.rs found: $name ----------"
+        fi
+
+        ${pkgs.rustc}/bin/rustc --crate-type=lib -g ''${S}lib.rs  ${depsString} --crate-name ${nameFix} --cap-lints "allow"  -L dependency=mylibs --cfg "feature=\"default\"" --cfg "feature=\"std\"" --out-dir $OUT_DIR/
+
+
+#36[pid  1128] execve("/nix/store/fff3jbf9vbqhmf6qjrmzhliq516x7yrf-rustc-1.11.0/bin/rustc", ["rustc", "/home/joachim/.cargo/registry/src/github.com-1ecc6299db9ec823/serde-0.8.19/src/lib.rs", "--crate-name", "serde", "--crate-type", "lib", "-g", "--cfg", "feature=\"default\"", "--cfg", "feature=\"std\"", "-C", "metadata=97f01bf227222121", "-C", "extra-filename=-97f01bf227222121", "--out-dir", "/home/joachim/Desktop/projects/fractalide/fetchUrl/hello_flate2/target/debug/deps", "--emit=dep-info,link", "-L", "dependency=/home/joachim/Desktop/projects/fractalide/fetchUrl/hello_flate2/target/debug/deps", "-L", "dependency=/home/joachim/Desktop/projects/fractalide/fetchUrl/hello_flate2/target/debug/deps", "--cap-lints", "allow"], [/* 105 vars */] <unfinished ...>
+# 
+# {
+#   "name": "serde",
+#   "vers": "0.8.19",
+#   "deps": [
+#     {
+#       "name": "clippy",
+#       "req": "0.*",
+#       "features": [],
+#       "optional": true,
+#       "default_features": true,
+#       "target": null,
+#       "kind": "normal"
+#     }
+#   ],
+#   "cksum": "58a19c0871c298847e6b68318484685cd51fa5478c0c905095647540031356e5",
+#   "features": {
+#     "std": [],
+#     "alloc": [
+#       "unstable"
+#     ],
+#     "unstable-testing": [
+#       "clippy",
+#       "unstable",
+#       "std"
+#     ],
+#     "unstable": [],
+#     "default": [
+#       "std"
+#     ],
+#     "collections": [
+#       "alloc"
+#     ]
+#   },
+#   "yanked": false
+# }
+
+
       '';
       # FIXME refactor this to use $lib and $src
       installPhase = ''
+        echo "----- $name install Phase-----"
+        du -a
+        echo "----- $name install Phase-----"
         mkdir $out
-        cp -Rf . $out
+#         cp -Rf . $out
+        cp -R $OUT_DIR/* $out
       '';
     };
 
