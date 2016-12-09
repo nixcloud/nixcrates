cdf320e3528e7a553126fa9064a3f08a89826c3e
Paul Seitz <paul.m.seitz@gmail.com>
2016-12-09 09:03:09 +0000

    debugging stuff

--- nix-crates-index/all-carg-packages.nix
+++ nix-crates-index/all-carg-packages.nix
@@ -17,6 +17,8 @@ let
     nameFix = normalizeName name;
     cratesDeps = pkgs.lib.fold ( recursiveDeps : newCratesDeps: newCratesDeps ++ recursiveDeps.cratesDeps  ) deps deps;
     depsString = pkgs.lib.fold ( dep: str: "${str} --extern ${normalizeName dep.name}=${dep}/lib${normalizeName dep.name}.rlib") "" deps;
+    symlinkCalc = pkgs.lib.fold ( dep: str: "${str} ln -sf ${dep}/lib${normalizeName dep.name}.rlib mylibs/ \n") "mkdir mylibs\n ";
+
   in
     stdenv.mkDerivation {
       name = name;
@@ -31,6 +33,10 @@ let
         cd ${name}-${version}
       '';
       buildPhase = ''
+
+        echo "${name} - er${depsString}"
+        ${symlinkCalc cratesDeps}
+        du -ha
         echo "name ${name}"
         # ugly hack since some creates use src/ and others don't! what a mess!
         if [ -d src ]; then
@@ -39,7 +45,8 @@ let
           S=""
         fi
 #        ${pkgs.rustc}/bin/rustc --crate-type=lib ''${S}lib.rs ${depsString} -o lib${nameFix}.rlib --crate-name ${nameFix} --cfg "feature=\"unsupported\"" --cfg "feature=\"default\""
-        ${pkgs.rustc}/bin/rustc --crate-type=lib ''${S}lib.rs -o lib${nameFix}.rlib --crate-name ${nameFix} --cfg "feature=\"unsupported\"" --cfg "feature=\"default\""
+#        ${pkgs.rustc}/bin/rustc --crate-type=lib ''${S}lib.rs -o lib${nameFix}.rlib --crate-name ${nameFix} --cfg "feature=\"unsupported\"" --cfg "feature=\"default\"" -L dependency=mylibs
+        ${pkgs.rustc}/bin/rustc --crate-type=lib ''${S}lib.rs -o lib${nameFix}.rlib --crate-name ${nameFix} --cfg "feature=\"use_std\"" -L dependency=mylibs
       '';
       # FIXME refactor this to use $lib and $src
       installPhase = ''
